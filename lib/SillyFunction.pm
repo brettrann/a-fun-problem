0e8fe2b7f02d00b0457954f38be4499a93ad0b90
Brett Rann <brettrann@gmail.com>
2014-09-11 08:53:50 +1000

    refactor indenting and add some clarity

--- lib/SillyFunction.pm
+++ lib/SillyFunction.pm
@@ -12,23 +12,22 @@
 package SillyFunction;
 
 sub group_products {
-my $products = shift;
-my %brand_type = ();
-my $grouped_products = [];
+  my $products = shift;
+  my %brand_type = ();
+  my $grouped_products = ();
 
-foreach (@{$products})
-{
-$brand_type{$_->{brand}} ||= {};
-$brand_type{$_->{brand}}->{$_->{type}} = 1;
-}
-foreach (sort keys %brand_type)
-{
-my $brand = $_;
-foreach (sort keys %{$brand_type{$brand}}) {
-push(@{$grouped_products}, { brand => $brand, type => $_});
-}
-}
-$grouped_products;
+  foreach my $product (@$products)
+  {
+    $brand_type{$product->{brand}}{$product->{type}} = 1;
+  }
+  foreach my $brand (sort keys %brand_type)
+  {
+    foreach my $type (sort keys %{$brand_type{$brand}})
+    {
+      push(@grouped_products, { brand => $brand, type => $type});
+    }
+  }
+  return \@grouped_products;
 }
 
 1;
